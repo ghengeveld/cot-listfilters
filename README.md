bae448eb8d4553d5d09a429335c6cefee3792d52
Gert Hengeveld <info@ghengeveld.nl>
2011-11-16 00:32:11 +0100

    Add README. Fix form helpers. Add docblocks.

---
+++ README.md
@@ -0,0 +1,101 @@
+List Filters 
+============
+
+List Filters adds advanced filtering options for page lists to Cotonti. A list 
+of available filter types is below.
+
+Installation
+------------
+
+Simply install the plugin through the admin panel as you would with any other 
+plugin. No configuration is required, just add the filters to your page.list.tpl 
+file. Implementation details are below. You may modify the plugin rights to 
+disable it for certain groups, by default all guests and members can use it.
+
+Filter types
+------------
+
+* eq	Equals (page_$field = $value) Value can be any string or number.
+* ne	Not Equal (page_$field != $value) Value can be any string or number.
+* lt	Less Than (page_$field < $value) Value must be numeric.
+* lte	Less Than or Equal (page_$field <= $value) Value must be numeric.
+* gt	Greater Than (page_$field > $value) Value must be numeric.
+* gte	Greater Than or Equal (page_$field >= $value) Value must be numeric.
+* in	SQL IN operator (page_$field IN ($value1, $value2, $value3)) Values must be comma seperated.
+* rng	SQL BETWEEN operator (page_$field BETWEEN $value1 AND $value2) Values must be seperated with a tilde (1~2). Strings are supported.
+
+Implementation
+--------------
+
+Implementation of List Filters is a matter of modifying your page.list.tpl to 
+include the filters. There are two ways in which you can display the filters: as
+plain text links or as a form. Which you choose depends on your use case. More 
+complex filtering systems are easier implemented using forms, while plain links 
+are more user friendly and quicker because there's no need to submit a form. The
+plugin provides helper functions for both implementation methods.
+
+Using links
+~~~~~~~~~~~
+
+For links the plugin provides the helper function listfilter_url(). This 
+function takes three arguments and will return a URL to the list with the 
+correct parameters for the filter. The arguments are filter type (see above), 
+field name (database column name without 'page_') and the value to apply. An 
+example:
+
+    listfilter_url('eq', 'exttype', 'modules')
+
+Assuming we're displaying the 'extensions' category, this will return:
+
+    index.php?e=page&c=extensions&filters[eq][exttype]=modules
+
+It's also possible to leave out the last argument (the value). This will return 
+a URL for the current list without this filter, effectively disabling it. Any 
+other filters will still be included in the link.
+
+Another function that is convenient is listfilter_active() which returns TRUE or
+FALSE depending on whether or not the filter is currently active. It takes the 
+exact same arguments as listfilter_url().
+
+Here's a complete example that uses these functions as CoTemplate callbacks:
+
+    <li><a href="{PHP|listfilter_url('eq', 'exttype')}"<!-- IF {PHP|listfilter_active('eq', 'exttype')} --> class="selected"<!-- ENDIF -->>{PHP.L.All}</a></li>
+    <li><a href="{PHP|listfilter_url('eq', 'exttype', 'modules')}"<!-- IF {PHP|listfilter_active('eq', 'exttype', 'modules')} --> class="selected"<!-- ENDIF -->>{PHP.L.Modules}</a></li>
+    <li><a href="{PHP|listfilter_url('eq', 'exttype', 'plugins')}"<!-- IF {PHP|listfilter_active('eq', 'exttype', 'plugins')} --> class="selected"<!-- ENDIF -->>{PHP.L.Plugins}</a></li>
+
+As you see this filter has three options: All, Modules and Plugins. In this case 
+'All' simply disables the filter. It's not a requirement to include a 'disable' 
+link, because clicking an active filter will disable it too (like a toggle 
+switch).
+
+Using a form
+~~~~~~~~~~~~
+
+An alternative to using plain text links is to use a form. This can be more
+convenient in complex situations. To simplify creating the form, the plugin 
+provides several helper functions for generating form elements. These functions 
+are effectively wrappers for the functions included in the Cotonti Forms API 
+(system/forms.php). They return complete HTML form fields. Provided functions 
+are:
+
+* listfilter_form_checkbox($type, $field, $value, $default = 0, $title = NULL)
+* listfilter_form_inputbox($type, $field, $default = '')
+* listfilter_form_radiobox($type, $field, $options, $default = '', $titles = NULL)
+* listfilter_form_selectbox($type, $field, $options, $default = '', $titles = NULL)
+
+* $type (string) One of the filter types listed above.
+* $field (string) Field name without 'page_' prefix.
+* $value (string) Value which the filter will check for.
+* $title (string) Text that will be displayed as a label (optional, defaults to $value).
+* $default (string) Default text to display in the input field (optional) or value that is selected by default or in case of a checkbox: 0 (not checked) or 1 (checked).
+* $options (string) Comma-seperated list of options.
+* $titles (string) Comma-seperated list of titles (optional, defaults to $values).
+
+Other helpers
+~~~~~~~~~~~~~
+
+listfilter_urlparam()
+
+Returns the URL query parameter for all currently active filters. Example:
+
+    filters[eq][a]=b&filters[eq][c]=d&filters[ne][e]=f
--- listfilters/listfilters.page.list.query.php
+++ listfilters/listfilters.page.list.query.php
@@ -10,7 +10,10 @@ defined('COT_CODE') or die('Wrong URL');
 if ($sqlfilters)
 {
 	$where['filter'] = $sqlfilters;
-	$params = array_merge($params, $sqlparams);
+	if (is_array($sqlparams))
+	{
+		$params = array_merge($params, $sqlparams);
+	}
 }
 
 if ($filters)
@@ -19,12 +22,31 @@ if ($filters)
 	$list_url = cot_url('page', $list_url_path);
 }
 
+/**
+ * Returns TRUE if the filter is active and meets the provided value.
+ * Returns TRUE if the filter is not active, if the last argument is omitted.
+ * 
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $value Value that was filtered on (optional)
+ * @return bool
+ */
 function listfilter_active($type, $field, $value = NULL)
 {
 	global $list_url_path;
 	return ($list_url_path['filters'][$type][$field] == $value);
 }
 
+/**
+ * Returns the current URL with query parameters for this filter added to it, or
+ * if the last argument is omitted, the current URL with this filter removed 
+ * from it.
+ * 
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $value Value to filter on (optional)
+ * @return string
+ */
 function listfilter_url($type, $field, $value = NULL)
 {
 	global $list_url_path;
@@ -40,51 +62,98 @@ function listfilter_url($type, $field, $value = NULL)
 	return cot_url('page', $params);
 }
 
+/**
+ * Returns the URL query parameter for all currently active filters.
+ * 
+ * @return string
+ */
 function listfilter_urlparam()
 {
 	global $list_url_path;
 	return http_build_query(array('filters' => $list_url_path['filters']));
 }
 
-function listfilter_form_checkbox($type, $field, $value, $title = NULL)
+/**
+ * Wrapper for cot_checkbox()
+ *
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $value Value to filter on
+ * @param int $default Default checked state (0 or 1)
+ * @param string $title Alternative label title (defaults to $value)
+ * @return string
+ */
+function listfilter_form_checkbox($type, $field, $value, $default = 0, $title = NULL)
 {
 	global $list_url_path;
 	if (!function_exists('cot_checkbox')) include cot_incfile('forms');
 	if ($title === NULL) $title = $value;
-	$chosen = ($list_url_path['filters'][$type][$field] == $value);
+	$chosen = (isset($list_url_path['filters'][$type][$field])) ?
+		($list_url_path['filters'][$type][$field] == $value) : (bool)$default;
 	return cot_checkbox($chosen, "filters[$type][$field]", $title, 'id="filter_'.$type.'_'.$field.'"', $value);
 }
 
-function listfilter_form_inputbox($type, $field, $values, $titles = '')
+/**
+ * Wrapper for cot_inputbox()
+ *
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $default Default value for the input field
+ * @return string
+ */
+function listfilter_form_inputbox($type, $field, $default = '')
 {
 	global $list_url_path;
 	if (!function_exists('cot_inputbox')) include cot_incfile('forms');
 	$value = $list_url_path['filters'][$type][$field];
+	if (!$value) $value = $default;
 	return cot_inputbox('text', "filters[$type][$field]", $value, 'id="filter_'.$type.'_'.$field.'"');
 }
 
-function listfilter_form_radiobox($type, $field, $values, $titles = '')
+/**
+ * Wrapper for cot_radiobox()
+ *
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $options Comma-seperated list of options
+ * @param string $default Option selected by default
+ * @param string $titles Comma-seperated list of alternative label titles (defaults to $options)
+ * @return string
+ */
+function listfilter_form_radiobox($type, $field, $options, $default = '', $titles = NULL)
 {
 	global $list_url_path;
 	if (!function_exists('cot_radiobox')) include cot_incfile('forms');
-	$values = explode(',', $values);
-	$titles = explode(',', $titles);
-	if (count($values) != count($titles)) $titles = $values;
+	$options = explode(',', $options);
+	$titles = ($titles !== NULL && count($options) == count(explode(',', $titles))) ?
+		explode(',', $titles) : $options;
 	$chosen = $list_url_path['filters'][$type][$field];
+	if (!$chosen) $chosen = $default;
 	if ($type == 'in') $chosen = explode(',', $chosen);
-	return cot_radiobox($chosen, "filters[$type][$field]", $values, $titles, true, 'id="filter_'.$type.'_'.$field.'"');
+	return cot_radiobox($chosen, "filters[$type][$field]", $options, $titles, true, 'id="filter_'.$type.'_'.$field.'"');
 }
 
-function listfilter_form_selectbox($type, $field, $values, $titles = '')
+/**
+ * Wrapper for cot_selectbox()
+ *
+ * @param string $type Filter type
+ * @param string $field Field name
+ * @param string $options Comma-seperated list of options
+ * @param string $default Option selected by default
+ * @param string $titles Comma-seperated list of alternative label titles (defaults to $options)
+ * @return string
+ */
+function listfilter_form_selectbox($type, $field, $options, $default = '', $titles = NULL)
 {
 	global $list_url_path;
 	if (!function_exists('cot_selectbox')) include cot_incfile('forms');
-	$values = explode(',', $values);
-	$titles = explode(',', $titles);
-	if (count($values) != count($titles)) $titles = $values;
+	$options = explode(',', $options);
+	$titles = ($titles !== NULL && count($options) == count(explode(',', $titles))) ?
+		explode(',', $titles) : $options;
 	$chosen = $list_url_path['filters'][$type][$field];
+	if (!$chosen) $chosen = $default;
 	if ($type == 'in') $chosen = explode(',', $chosen);
-	return cot_selectbox($chosen, "filters[$type][$field]", $values, $titles, true, 'id="filter_'.$type.'_'.$field.'"');
+	return cot_selectbox($chosen, "filters[$type][$field]", $options, $titles, true, 'id="filter_'.$type.'_'.$field.'"');
 }
 
 ?>
